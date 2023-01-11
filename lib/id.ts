4ee0b562397a760d20aeb618119a360eb080b488
kaplosh <kaplanekt@gmail.com>
2023-01-11 10:10:57 +0100

    array of used id's

--- lib/id.ts
+++ lib/id.ts
@@ -1,10 +1,16 @@
+const usedIds: Array<string> = [];
 
 export function onNewId () {
   let newID = '';
   for (let step = 0; step < 5; step++) {
-    const partOfId = Math.floor(Math.random() * 10);
+    const partOfId = String(Math.floor(Math.random() * 10));
     newID += partOfId;
   }
+  usedIds.push(newID);
+
+  console.log(newID);
+  console.log(usedIds);
+
   return newID;
 }
 
