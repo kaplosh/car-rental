97385a42cba2830ed91d79de51859bd6505005e7
doooby <zelazk.o@email.cz>
2023-02-02 13:17:36 +0100

    code review

--- pages/home.vue
+++ pages/home.vue
@@ -11,20 +11,16 @@ export default {
 <template>
   <div class="container">
     <div class="d-flex mt-5 justify-content-center">
-      <main class="d-flex flex-column">
-        <header class="mb-4 align-self-center">
+      <main class="d-flex flex-column align-items-center">
+        <header class="mb-4">
           Kaplosh's Car Rental
         </header>
-        <div class="align-self-center ">
           <NuxtLink to="/cars/listing" class="btn btn-outline-primary mb-3">
             Car list
           </NuxtLink>
-        </div>
-        <div class="align-self-center ">
           <NuxtLink to="" class="btn btn-outline-primary mb-3">
             Customers
           </NuxtLink>
-        </div>
       </main>
     </div>
   </div>
